3915562efd4a8762384a5c17c5f83dd9fa4d37cf
zuuXs <abd-rezak.amiar@etu.u-pec.fr>
2023-07-08 04:07:55 +0200

    Project Initiation

---
+++ src/main/java/org/openlab/openlabcustomerservice/OpenlabCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.openlab.openlabcustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlabCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenlabCustomerServiceApplication.class, args);
+	}
+
+}
